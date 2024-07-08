75a8bdc7666afbca88975dae66e59b4153b2afd7
Jan Zwiener <jan@zwiener.org>
2024-07-08 23:17:07 +0200

    Be clear that we overwrite the current init.vim

--- initneovim.bat
+++ initneovim.bat
@@ -4,9 +4,11 @@ set "CONFIG_DIR=%LOCALAPPDATA%\nvim"
 if not exist "%CONFIG_DIR%" (
     mkdir "%CONFIG_DIR%"
 )
-
+echo "Overwriting init.vim..."
+pause
 REM Create the init.vim file with the required content
 set "INIT_VIM=%CONFIG_DIR%\init.vim"
 (
 echo source %HOMEDRIVE%%HOMEPATH%\vimfiles\vimrc
 ) > "%INIT_VIM%"
+echo Run :NeovideRegisterRightClick within Neovide
